8435b490d6ed133c559ddb666c17703ebace2d94
DeaVenditama <deavenditama@gmail.com>
2020-08-28 03:00:25 +0700

    part 3

--- app/Controllers/Maps.php
+++ app/Controllers/Maps.php
@@ -4,7 +4,33 @@ class Maps extends BaseController
 {
 	public function index()
 	{
-		return view('maps/index');
+		$model = new \App\Models\DataModel();
+
+		$fileName = base_url("maps/prov.geojson");
+		$file = file_get_contents($fileName);
+		$file = json_decode($file);
+
+		$features = $file->features;
+
+		foreach($features as $index=>$feature)
+		{
+			$kode_wilayah = $feature->properties->kode;
+			$data = $model->where('id_master_data',1)
+						->where('kode_wilayah', $kode_wilayah)
+						->first();
+
+			if($data)
+			{
+				$features[$index]->properties->nilai = $data->nilai;
+			}
+		}
+
+		$nilaiMax = $model->select('MAX(nilai) AS nilai')->where('id_master_data', 1)->first()->nilai;
+
+		return view('maps/index',[
+			'data' => $features,
+			'nilaiMax' => $nilaiMax,
+		]);
 	}
 
 	//--------------------------------------------------------------------
--- app/Views/maps/index.php
+++ app/Views/maps/index.php
@@ -17,14 +17,47 @@
 
 <?= $this->section('script') ?>
 <script>
+    var data = <?= json_encode($data) ?>;
+    var nilaiMax = <?= $nilaiMax ?>;
+
     var map = L.map('maps').setView({ lat : 0.7893, lon : 113.9213 }, 5);
+    
+    function getColor(d) {
+		return d > (nilaiMax/8)*7 ? '#800026' :
+           d > (nilaiMax/8)*6  ? '#BD0026' :
+           d > (nilaiMax/8)*5  ? '#E31A1C' :
+           d > (nilaiMax/8)*4  ? '#FC4E2A' :
+           d > (nilaiMax/8)*3   ? '#FD8D3C' :
+           d > (nilaiMax/8)*2   ? '#FEB24C' :
+           d > (nilaiMax/8)*1   ? '#FED976' :
+                      '#FFEDA0';
+    }
+    
+    function style(feature){
+        return{
+            weight : 2,
+            opacity : 1,
+            color : 'white',
+            dashArray : '3',
+            fillOpacity : 0.7,
+            fillColor : getColor(parseInt(feature.properties.nilai))
+        };
+    }
+
+    function onEachFeature(feature, layer)
+    {
+        layer.bindPopup("<h4>Jumlah Proyeksi Penduduk</h4><br>"+feature.properties.Propinsi+" : "+feature.properties.nilai +"(000) ribu jiwa");
+    }
+
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="https://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
     }).addTo(map);
 
-    L.marker({lat : 0.7893, lon : 113.9213}).bindPopup('Hello Indonesia').addTo(map);
-
+    var geojson = L.geoJson(data, {
+        style : style,
+        onEachFeature : onEachFeature
+    }).addTo(map);
 </script>
 <?= $this->endSection() ?>
